19bd80cd60e5c160c7990de44981b70067cfedcf
Bartosz Tarnawski <bartek.tarnawski@gmail.com>
2014-12-12 21:34:58 +0100

    Lepsze rymy

--- scripts/rhymes.py
+++ scripts/rhymes.py
@@ -16,31 +16,45 @@ def get_end(word, span):
 
     return word[i:]
 
+
+def rhymes_with(c1, c2, span):
+    w1 = normalize(c1)
+    w2 = normalize(c2)
+    if len(w1) < 3 or len(w2) < 3:
+        return False
+    if w1 in w2 or w2 in w1:
+        return False
+    return get_end(w1, span) == get_end(w2, span)
+      
+
+
 class Rhymes:
     def __init__(self, words, span):
         self.words = list(words)
+        self.span = span
+        """
         self.rhymes = defaultdict(set)
         for w in self.words:
             #if w[-1] in ('.', ','):
             end = get_end(normalize(w), span)
             self.rhymes[end].add(w)
         self.rhymes = [list(l) for (k, l) in self.rhymes.items() if len(l) > 1]
+        """
 
     def random_rhyme(self):
         c1 = 'a'
         c2 = 'a'
-        while normalize(c1) == normalize(c2):
-            choices = random.choice(self.rhymes)[:]
-            random.shuffle(choices)
-            c1 = choices[0]
-            c2 = choices[1]
+        while not rhymes_with(c1, c2, self.span):
+            c1 = random.choice(self.words)
+            c2 = random.choice(self.words)
         return (c1, c2)
 
 
 
 def from_file(words, span):
     rhs = Rhymes(words, span)
-    for v in rhs.rhymes:
+    return rhs
+    """for v in rhs.rhymes:
         if len(v) > 5:
             print(v)
-    return rhs
+    return rhs"""
